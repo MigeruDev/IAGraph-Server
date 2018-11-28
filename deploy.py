f69dd41f209eb91adb6a22ddbfbb92bc19ac36f0
Miguel Ángel <mangelladen@gmail.com>
2018-11-28 14:07:28 -0500

    'debug'

--- deploy.py
+++ deploy.py
@@ -88,5 +88,6 @@ def upload_file():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.debug = True
     #app.run(host='127.0.0.1',port=port) #for local
     app.run(host='0.0.0.0', port=port) #for Heroku
